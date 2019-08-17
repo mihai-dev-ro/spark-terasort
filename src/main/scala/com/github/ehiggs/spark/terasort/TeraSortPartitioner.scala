32d22244e1b75429d9decfe50595075e076e6deb
Mihai Nicola <mihai.nicolla@gmail.com>
2019-08-17 18:46:25 +0300

    add debug lines

--- src/main/scala/com/github/ehiggs/spark/terasort/TeraSortPartitioner.scala
+++ src/main/scala/com/github/ehiggs/spark/terasort/TeraSortPartitioner.scala
@@ -34,7 +34,10 @@ case class TeraSortPartitioner(numPartitions: Int) extends Partitioner {
   override def getPartition(key: Any): Int = {
     val b = key.asInstanceOf[Array[Byte]]
     val prefix = Longs.fromBytes(0, b(0), b(1), b(2), b(3), b(4), b(5), b(6))
-    (prefix / rangePerPart).toInt
+    val partition = (prefix / rangePerPart).toInt
+    println(s"rangePerPart: $rangePerPart, partition: $partition, prefix: $prefix")
+
+    partition
   }
 }
 
