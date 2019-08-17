4b5a4b0de1e7763d4e457ddffcf02c5fffb933ae
Mihai Nicola <mihai.nicolla@gmail.com>
2019-08-17 19:31:15 +0300

    use the default parallize for partitioning

--- src/main/scala/com/github/ehiggs/spark/terasort/TeraSortPartitioner.scala
+++ src/main/scala/com/github/ehiggs/spark/terasort/TeraSortPartitioner.scala
@@ -34,10 +34,7 @@ case class TeraSortPartitioner(numPartitions: Int) extends Partitioner {
   override def getPartition(key: Any): Int = {
     val b = key.asInstanceOf[Array[Byte]]
     val prefix = Longs.fromBytes(0, b(0), b(1), b(2), b(3), b(4), b(5), b(6))
-    val partition = (prefix / rangePerPart).toInt
-    println(s"rangePerPart: $rangePerPart, partition: $partition, prefix: $prefix")
-
-    partition
+    (prefix / rangePerPart).toInt
   }
 }
 
