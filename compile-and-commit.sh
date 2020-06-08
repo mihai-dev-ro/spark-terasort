8357d1a200dfe053f2ad16ee2ab209e0121ec34c
Mihai Nicola <liviu-mihai.nicola@ing.com>
2020-06-08 17:18:27 +0300

    search solution for dependency libraries integration bug

--- compile-and-commit.sh
+++ compile-and-commit.sh
@@ -1,5 +1,5 @@
 mvn package
-cp target/spark-terasort-1.1-SNAPSHOT-jar-with-dependencies.jar .
+cp target/spark-terasort-1.1-SNAPSHOT.jar .
 git add .
 git commit -m "search solution for dependency libraries integration bug"
 git push origin master
